fd4b60785b8e89b24101fb69dd0e2ce035865ede
GoZaddy <faruqyusuff437@gmail.com>
2021-06-21 22:01:56 -0500

    fixed problems that occurred while updating repo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
+//  class Node {
+//     constructor(value) {
+//         this.value = value
+//         this.children = []
+//     }
+//
+//     addChild(childNode){
+//         this.children.push(childNode)
+//     }
+//
+// }
+//
+// class Tree {
+//     constructor(rootNode) {
+//         this.root = rootNode
+//     }
+// }
+//
+// class Cell{
+//     constructor(name) {
+//         this.name = name
+//
+//         // can only be occupied by 'x' or 0
+//         this.occupiedBy = null
+//     }
+//
+//     isValidCell(){
+//         return 'abc'.includes(this.name[0]) && '123'.includes(this.name['x'])
+//     }
+//
+// }
+
 class Board {
     constructor() {
         //this.playedMoves = []
@@ -44,6 +76,7 @@ class Board {
                 'b': 2,
                 'c': 3
             }
+            console.log(letterToNum[cell.name.toLowerCase()[0]] + parseInt(cell.name['x']))
             return letterToNum[cell.name.toLowerCase()[0]] + parseInt(cell.name['x'])
         }
         let score = 0
@@ -61,10 +94,18 @@ class Board {
         this._cells = cells
     }
 
+    // // evaluate for o
+    //
+    // if (this.getCell(winCells[i]).occupiedBy === 0){
+    //     currentOScore += 'x'
+    // } else if (this.getCell(winCells[i]).occupiedBy === 'x'){
+    //     currentOScore = 0;
+    // }
     evaluateGameState(){
         let XScore = 0
         let OScore = 0
         this._winCells.forEach(winCells => {
+            // console.log("For trio", winCells)
             let currentXScore = 0
             let currentOScore = 0
             for (let i = 0; i < winCells.length; i++){
@@ -107,13 +148,27 @@ class Board {
 
         })
         if (XScore === Number.POSITIVE_INFINITY && OScore === Number.NEGATIVE_INFINITY){
+            console.log(this._cells)
             throw "an error occurred somewhere";
         }
+        // console.log('o score: ', OScore)
+        // console.log('x score: ', XScore)
         return XScore +OScore;
+    }
+
+    hasOpenCell(){
+        let openCells = 0;
+        Object.keys(this._cells).forEach(cell => {
+            if(!this._cells[cell].occupiedBy){
+                openCells += 1
+            }
+        })
+        console.log('number of open cells', openCells)
+        return openCells !== 0
     }
 }
 
-
+//T0DO: write position evaluation function
 
  function alternate(str){
     if (str === 'x'){
@@ -121,6 +176,7 @@ class Board {
     } else if (str === 'o'){
         return 'x'
     } else{
+        console.log("str is ", str)
         return -1
     }
  }
@@ -141,29 +197,37 @@ class Board {
      if (gameState === Number.POSITIVE_INFINITY || gameState === Number.NEGATIVE_INFINITY){
          return {val: gameState}
      }
-    if (depth === 0){
+    if (depth === 0 || !b.hasOpenCell()){
         const b = new Board();
         b.setCells(boardCells)
+        //console.log(b.cells())
         const gameState = b.evaluateGameState()
         if (gameState === undefined){
-            //console.log('error occurred')
+            //console.log('is here o')
         }
+
+        //this is synonymous to reaching rhe leaf nodes and giving them values using the heuristic function
         return {val: gameState}
     }
 
     const res = {}
     const res1 = [];
-    //let count = 0
     Object.keys(boardCells).forEach(cell => {
         // we will now run minimax again for the unfilled cells - might delete this later so don't commit to it
          if (boardCells[cell].occupiedBy === null){
-             const minmax = minimax(alternate(playerID), {
-                 ...boardCells,
-                 [cell]: {occupiedBy: playerID}
-             }, depth - 1, {
-                 move: playerID.toString() + ' plays ' + cell
-             })
+             const minmax = minimax(
+                 alternate(playerID),
+                 {
+                     ...boardCells,
+                     [cell]: {...boardCells[cell], occupiedBy: playerID}
+                 },
+                 depth - 1,
+                 {
+                    move: playerID.toString() + ' plays ' + cell
+                 }
+             )
 
+             // this might be a fatal error because we can't assign scores based on just one cell. A final score is the result of different plays. Right? Actually no, since minimax only helps us minimise loss and maximise win and not predict opponent's actual moves
              res[minmax.val] = cell;
              res1.push({
                  [cell]: minmax.val
@@ -171,6 +235,10 @@ class Board {
             //count += 1
          }
     })
+
+     // console.log('values returned from minimax: ', res)
+     // console.log('values returned from minimax 1: ', res1)
+     // console.log("count: ", count)
 
      if(res === {}){
          return minimax(playerID, boardCells, 0, metadata)
@@ -183,13 +251,14 @@ class Board {
          const max = Math.max(...Object.keys(res));
          return {val: max, move: res[max]};
      } else{
+         //console.log(playerID);
          throw "some error occurred";
      }
 
 
 
  }
-
+ //console.log('result', minimax('o', b.cells(), 4, {}));
 
  export {
      Board,
